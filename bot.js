3d8db869b7fa877140d5876daa4e66deb0e1c057
Danny Beyrer <beyrerdn@gmail.com>
2018-05-30 11:37:03 +0800

    bot using twit library API

--- bot.js
+++ bot.js
@@ -8,4 +8,33 @@ var retweet = function () {
     result_type: 'recent',
     lang: 'en'
   }
+
+  Twitter.get('search/tweets', params, function(err, data) {
+      // if there no errors
+        if (!err) {
+          // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong while SEARCHING...');
+        }
+    });
+
 }
+
+retweet();
+
+setInterval(retweet, 3000000);
