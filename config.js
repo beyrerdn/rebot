0c7cfb4b17dcfab0de0a77403edfea1fbfd63837
Danny Beyrer <beyrerdn@gmail.com>
2018-05-31 11:50:22 +0800

    switch env vars

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: ENV['CONSUMER_KEY'] || process.env.CONSUMER_KEY,
-  consumer_secret: ENV['CONSUMER_SECRET'] || process.env.CONSUMER_SECRET,
-  access_token: ENV['ACCESS_TOKEN'] || process.env.ACCESS_TOKEN,
-  access_token_secret: ENV['ACCESS_TOKEN_SECRET'] || process.env.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
